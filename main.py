2eddbd756894420a97489e311f2a4f4cb00f45ad
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-04-25 00:36:26 +0200

    add command to return most frequent subsequence from given string

--- main.py
+++ main.py
@@ -31,4 +31,34 @@ async def palindrome(ctx: commands.Context, *args):
     res = {arg: 'Yes' if arg == arg[::-1] else 'No' for arg in args }
     await ctx.send(json.dumps(res, indent=4))
 
+@bot.command(name='sub_sequence_frequency')
+async def most_frequent_sub_sequence(ctx: commands.Context, arg: str, count: str):
+
+    if arg == '':
+        return await ctx.reply('Is this funny ? The sequence is empty.')
+
+    size = int(count)    
+
+    if size >= len(arg):
+        return await ctx.reply('The subsequence must have lower size than the sequence.')
+    
+    if size == 0:
+        return await ctx.reply('What\'s the point of doing this?')
+
+    res = {}
+
+    for i in range(len(arg) - (size - 1)):
+        sub: str = arg[i:i+size]
+        freq: int = res.get(sub) or 0
+        res[sub]: int = freq + 1
+
+    most_frequent_name: str = ''
+    most_frequent_value: int = 0
+
+    for item in res.items():
+        if item[1] > most_frequent_value:
+            most_frequent_name, most_frequent_value = item
+
+    await ctx.send(json.dumps({ most_frequent_name: most_frequent_value }, indent=4))
+
 bot.run(TOKEN)
