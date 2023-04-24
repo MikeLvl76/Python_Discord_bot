3700c6b168ece153c37a2e20d9dbb892566f781a
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-04-25 00:01:53 +0200

    add command for checking if words are palindrom

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+import json
 import discord, os
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+TOKEN: str = os.getenv('TOKEN')
+GUILD: int = int(os.getenv('GUILD_ID'))
+MAIN_CHANNEL: int = int(os.getenv('MAIN_CHANNEL_ID'))
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.get_channel(MAIN_CHANNEL).send('THE BOT OF ALL TIME IS HERE!')
+
+@bot.event
+async def on_message(message: discord.Message):
+    if not message.author.bot:
+        print(f'Message from {message.author}: {message.content}')
+    await bot.process_commands(message)
+
+@bot.command()
+async def palindrome(ctx: commands.Context, *args):
+    if len(args) == 0:
+        ctx.reply('Provide one or many words.')
+    res = {arg: 'Yes' if arg == arg[::-1] else 'No' for arg in args }
+    await ctx.send(json.dumps(res, indent=4))
+
+bot.run(TOKEN)
